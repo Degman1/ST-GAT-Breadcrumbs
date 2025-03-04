fix attention normalization

dataloader/breadcrumbs_dataloader.py
@@ -29,10 +29,11 @@ class BreadcrumbsDataset(DGLDataset):
         )
 
     def process(self):
-        data = pd.read_csv(self.raw_file_names[0], index_col=0, header=0).values
+        data = pd.read_csv(self.raw_file_names[0], index_col=0, header=0)
         if self.node_subset is not None:
             selected_node_ids = [str(node_id) for node_id in self.node_subset]
             data = data[selected_node_ids]
+        data = data.values
 
         # Leaving out normalization because most values in the dataset are 0 or 1
         self.mean = np.mean(data)
@@ -155,10 +156,10 @@ class BreadcrumbsDataset(DGLDataset):
         return len(self.graphs)
 
 
-def get_processed_dataset(config):
+def get_processed_dataset(config, node_subset=None):
     # Number of possible windows in a day
 
-    dataset = BreadcrumbsDataset(config, root="./dataset", force_reload=True)
+    dataset = BreadcrumbsDataset(config, root="./dataset", force_reload=True, node_subset=node_subset)
 
     d_mean = dataset.mean
     d_std_dev = dataset.std_dev

run.py
@@ -5,6 +5,7 @@ import importlib
 from dgl.dataloading import GraphDataLoader
 import os
 import pandas as pd
+import networkx as nx
 
 import dataloader.breadcrumbs_dataloader
 import dataloader.splits
@@ -14,6 +15,7 @@ import models.persist
 import visualizations.attention_matrix
 import visualizations.select_significant_pois
 import visualizations.predictions
+import visualizations.adjacency_matrix
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f"Using {device}")
@@ -25,25 +27,39 @@ torch.manual_seed(0)
 
 model_dir, runs_dir = models.trainer.setup_directories("Predicting_Breadcrumbs_Movement")
 
-RETRAIN = True
+RETRAIN = False
 SAVE_MODEL = True
 SAVE_ATTENTION = True
 
+if RETRAIN and SAVE_ATTENTION:
+    models.trainer.delete_files_in_directory("./attn_values")
+
 config = {
     'BATCH_SIZE': 50,
-    'EPOCHS': 150,
+    'EPOCHS': 80,
     'WEIGHT_DECAY': 5e-5,
-    'INITIAL_LR': 3e-4,
+    'INITIAL_LR': 3e-4, #-3
     'CHECKPOINT_DIR': runs_dir,
     'N_PRED': 9,
     'N_HIST': 12,
     'DROPOUT': 0.2,
     'USE_GAT_WEIGHTS': True,
-    # number of measurements per day
-    'SLOTS_PER_DAY': 24,
 }
 
-dataset, config['D_MEAN'], config['D_STD_DEV'], d_train, d_val, d_test = dataloader.breadcrumbs_dataloader.get_processed_dataset(config)
+node_subset = None
+# node_subset = [1, 13, 9, 25, 14, 5, 10, 35, 15, 106, 
+#                80, 7, 30, 8, 27, 51, 41, 49, 6, 29, 
+#                2, 90, 28, 17, 36, 33, 23, 86, 127, 105,
+#                107, 148, 111, 59, 11405, 147, 50, 55, 95, 16,
+#                18, 10863, 48, 215, 12, 142, 40, 10998, 158, 47,
+#                53, 84, 99, 21, 180, 10287, 45, 189, 10979, 79
+# ]
+#     68, 154, 26, 11538, 11866    52 11910   104 10305   176 10940    85
+#    188   207    32    22   156    91    76    57 10376     3 10901 10290
+#    110   134 11701   161   187   149   120   177   140 11397   164    61
+#    136    58    73   192]
+
+dataset, config['D_MEAN'], config['D_STD_DEV'], d_train, d_val, d_test = dataloader.breadcrumbs_dataloader.get_processed_dataset(config, node_subset)
 print("Completed Data Preprocessing.")
 
 if config['D_MEAN'] is not None:
@@ -59,7 +75,7 @@ config['N_NODE'] = dataset.graphs[0].number_of_nodes()
 attn_matrices_by_batch_by_epoch = None
 
 if SAVE_ATTENTION:
-    epochs_for_saving_attn = [0, config['EPOCHS'] - 1]
+    epochs_for_saving_attn = [config['EPOCHS'] - 1]
 else:
     epochs_for_saving_attn = []
 
@@ -93,16 +109,44 @@ print(f"Number of graphs in test dataset: {len(d_test)}")
 # Run inference on the test data
 _, _, _, y_pred, y_truth, _ = models.trainer.model_test(model, test_dataloader, device, config)
 
+# Build the attention matrix from the stored data.
 epoch = config['EPOCHS'] - 1
-attention = visualizations.attention_matrix.build_attention_matrices(dataset, config, epoch, "attn_values")
-np.save("attention_matrix.npy", attention[0])
-normalized_attn = visualizations.attention_matrix.plot_heatmap(attention[0], epoch, 1000, dataset.graphs[0].ndata["id"], 100)
-significant_pois, sorted_scores, sorted_indices = visualizations.select_significant_pois.get_significant_pois(normalized_attn, dataset.graphs[0].ndata["id"], plot=False)
+attention = visualizations.attention_matrix.build_attention_matrices(dataset, config, epoch, "attn_values")[0]
 
-print(significant_pois)
+# Normalize the attention with frobenius norm and save to file
+normalized_attention = attention / np.linalg.norm(attention, 'fro')
+np.save("normalized_attention.npy", normalized_attention)
 
-for i in range(30):
-    rank = i
-    prediction_node_index = sorted_indices[i]
-    node_label = significant_pois[i]
-    visualizations.predictions.plot_prediction(test_dataloader, y_pred, y_truth, prediction_node_index, node_label, rank, config)
+# Plot the attention as a heat map
+visualizations.attention_matrix.plot_heatmap(
+    normalized_attention, 
+    epoch, 
+    custom_node_ids=dataset.graphs[0].ndata["id"], 
+    display_step=100
+)
+
+# Compute POI rankings
+significant_pois, sorted_scores, sorted_indices = visualizations.select_significant_pois.get_significant_pois(
+    normalized_attention, 
+    dataset.graphs[0].ndata["id"], 
+    plot=True
+)
+
+print(f"Significant POIs:\n{significant_pois}")
+print(f"Corresponding Scores:\n{sorted_scores}")
+
+# Plot the subgraph of just the top ranked POIs
+# G = nx.read_adjlist("dataset/pruned_clustered_G3Hops.adjlist")
+# adj_mtx = nx.to_numpy_array(G)
+# subgraph_adj_mtx = visualizations.adjacency_matrix.get_subgraph_adjacency(adj_mtx, sorted_indices[:60])
+# visualizations.adjacency_matrix.plot_adjacency_matrix(subgraph_adj_mtx, "./visualizations/subgraph_adj_mtx.png")
+
+# Plot the attention matrix of just the top ranked POIs
+# subgraph_attn_mtx = visualizations.adjacency_matrix.get_subgraph_adjacency(normalized_attention, sorted_indices[:60])
+# visualizations.attention_matrix.plot_heatmap(subgraph_attn_mtx, epoch)
+
+# for i in range(30):
+#     rank = i
+#     prediction_node_index = sorted_indices[i]
+#     node_label = significant_pois[i]
+#     visualizations.predictions.plot_prediction_full(test_dataloader, y_pred, y_truth, prediction_node_index, node_label, rank, config)

visualizations/adjacency_matrix.py
@@ -24,8 +24,22 @@ def plot_adjacency_matrix(matrix, filename="adjacency_matrix.png"):
 
   plt.savefig(filename)
   plt.close()
-    
+
+def get_subgraph_adjacency(adj_matrix, node_indices):
+  """
+  Extracts the adjacency matrix for a subset of nodes.
+  
+  Parameters:
+  - adj_matrix (numpy.ndarray): The original adjacency matrix (N x N).
+  - node_indices (list or array): The indices of the nodes in the subgraph.
+  
+  Returns:
+  - sub_adj_matrix (numpy.ndarray): The adjacency matrix for the subgraph.
+  """
+  node_indices = np.array(node_indices)  # Ensure it's an array
+  return adj_matrix[np.ix_(node_indices, node_indices)]
+
 if __name__ == "__main__":
-    G = nx.read_adjlist("../dataset/pruned_clustered_G3Hops.adjlist")
+    G = nx.read_adjlist("../dataset/clustered_G3Hops.adjlist")
     adj_mtx = nx.to_numpy_array(G)
     plot_adjacency_matrix(adj_mtx)

visualizations/attention_matrix.py
@@ -26,7 +26,7 @@ def edge_attention_to_matrix(graph, edge_attention):
     return adj_matrix
 
 
-def build_attention_matrices(dataset, config, epoch, npz_dir):
+def build_attention_matrices(dataset, config, epoch, npz_dir, node_subset=None):
     graph = dataset.graphs[0]  # All graphs have the same structure, so we use the first graph for the structure
 
     # Collect attention matrices for each time slot
@@ -82,19 +82,17 @@ def build_attention_matrices(dataset, config, epoch, npz_dir):
     return attention_matrices
 
 
-def plot_heatmap(attn_matrix, epoch_number, time_slot, custom_node_ids=None, display_step=50):
+def plot_heatmap(attn_matrix, epoch_number, custom_node_ids=None, display_step=50):
     """
     Plot a heatmap of the attention matrix.
 
     :param attn_matrix: Node-to-node attention matrix.
     :param epoch_number: The current epoch number for labeling.
-    :param time_slot: The time slot associated with the attention matrix.
     :param custom_node_ids: Optional list of custom node IDs to display as axis labels.
     :param display_step: Step size to select custom node IDs for axis labels (default=50).
     """
-    attn_matrix_normalized = (attn_matrix - np.min(attn_matrix)) / (np.max(attn_matrix) - np.min(attn_matrix))
     plt.figure(figsize=(10, 8))
-    ax = sns.heatmap(attn_matrix_normalized, cmap="Blues", annot=False, fmt=".2f", cbar_kws={"shrink": 0.8})
+    ax = sns.heatmap(attn_matrix, cmap="Blues", annot=False, fmt=".2f", cbar_kws={"shrink": 0.8})
     plt.title(f"Attention Heatmap (Epoch {epoch_number})")
     
     if custom_node_ids is not None:
@@ -125,4 +123,3 @@ def plot_heatmap(attn_matrix, epoch_number, time_slot, custom_node_ids=None, dis
         bbox_inches="tight"
     )
     print(f"Attention heat map saved to {name}")
-    return attn_matrix_normalized

visualizations/select_significant_pois.py
@@ -21,4 +21,4 @@ def get_significant_pois(attention_matrix, customNodeIds=None, top_k=100, plot=T
         plt.savefig("visualizations/poi_significance.png", bbox_inches="tight")
         print("POI significance chart saved.")
 
-    return np.array(customNodeIds)[significant_pois] if customNodeIds is not None else significant_pois, sorted_scores, sorted_indices
+    return np.array(customNodeIds)[significant_pois] if customNodeIds is not None else significant_pois, sorted_scores[:top_k], sorted_indices